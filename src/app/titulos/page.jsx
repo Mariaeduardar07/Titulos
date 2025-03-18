25f255275e914f4d6f9ed9fa19ef26e11f0a1a1e
Maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2025-03-18 11:34:29 -0300

    feat: create section components

--- src/app/titulos/page.jsx
+++ src/app/titulos/page.jsx
@@ -3,15 +3,14 @@ import Header from "../components/header";
 import styles from "./titulos.module.css";
 import Footer from "../components/footer";
 import Card from "../components/cards";
+import Section from "../components/section";
  
  export default function Titulos() {
    return (
      <div className={styles.container}>
       <Header />
        <main className={styles.main}>
-         <section className={styles.featured}>
-           <h2>Conquistas Principais</h2>
-           <div className={styles.featuredTrophies}>
+         <Section titulo={"Conquistas Principais"}>
             <Card 
             ano={2012} 
             capa="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg" detalhes={"Corinthians 2 x 0 Boca Juniors"} 
@@ -29,57 +28,12 @@ import Card from "../components/cards";
             detalhes={"Corinthians 0(4) x 0(3) Vasco"}
             textoAlt={"Copa Libertadores da América"}
             titulo={"Copa Libertadores da América"} />
-           </div>
-         </section>
- 
-         <section className={styles.newTrophies}>
-           <h2>Campeonatos Brasileiros</h2>
-           <div className={styles.trophiesGrid}>
-             <div className={styles.trophy}>
-               <div className={styles.trophyCover}>
-                 <img
-                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                   alt="Troféu do Campeonato Brasileiro 2017"
-                 />
-               </div>
-               <div className={styles.trophyInfo}>
-                 <h3>Campeonato Brasileiro</h3>
-                 <p className={styles.year}>2017</p>
-                 <p className={styles.details}>7º título brasileiro</p>
-                 <button className={styles.detailsButton}>Ver Detalhes</button>
-               </div>
-             </div>
-             <div className={styles.trophy}>
-               <div className={styles.trophyCover}>
-                 <img
-                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                   alt="Troféu do Campeonato Brasileiro 2015"
-                 />
-               </div>
-               <div className={styles.trophyInfo}>
-                 <h3>Campeonato Brasileiro</h3>
-                 <p className={styles.year}>2015</p>
-                 <p className={styles.details}>6º título brasileiro</p>
-                 <button className={styles.detailsButton}>Ver Detalhes</button>
-               </div>
-             </div>
-             <div className={styles.trophy}>
-               <div className={styles.trophyCover}>
-                 <img
-                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                   alt="Troféu do Campeonato Brasileiro 2011"
-                 />
-               </div>
-               <div className={styles.trophyInfo}>
-                 <h3>Campeonato Brasileiro</h3>
-                 <p className={styles.year}>2011</p>
-                 <p className={styles.details}>5º título brasileiro</p>
-                 <button className={styles.detailsButton}>Ver Detalhes</button>
-               </div>
-             </div>
-           </div>
-         </section>
- 
+         </Section>
+        <Section />
+
+<section titulo={"Campeonato Brasilero"}>
+  
+</section>
          <section className={styles.promo}>
            <h2>Campeonatos Estaduais</h2>
            <div className={styles.trophiesGrid}>
